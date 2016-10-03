85a72992199dc4956e50fa3a387cf0e48ba4be3e
Asaf Atzmon <asaf.atzmon@gmail.com>
2016-10-03 21:50:20 +0300

    sep 2016

--- balance_app/admin.py
+++ balance_app/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Owner, Account, Category, Excluded_Category,Excluded_Action, Transaction
+from .models import Owner, Account, Category, Excluded_Category,Excluded_Action, Transaction, Transaction_to_Category, Word_to_Category
 
 admin.site.register(Owner)
 admin.site.register(Account)
@@ -10,4 +10,5 @@ admin.site.register(Category)
 admin.site.register(Excluded_Category)
 admin.site.register(Excluded_Action)
 admin.site.register(Transaction)
-
+admin.site.register(Transaction_to_Category)
+admin.site.register(Word_to_Category)
